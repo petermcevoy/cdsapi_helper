364606957eda20701d79eddbe539480f2db8ad13
Nils Müller <nilsmu@chalmers.se>
2025-10-13 16:08:24 +0200

    Remove arbitrary 'download_era5' script

--- cdsapi_helper/cli.py
+++ cdsapi_helper/cli.py
@@ -22,53 +22,12 @@ from .download import (
     RequestEntry,
 )
 from .utils import (
-    build_filename,
     build_request,
     resolve_and_get_local_cache,
     print_files_and_size,
     format_bytes,
     REQUEST_DATABASE_FILE,
 )
-
-
-@click.command()
-@click.argument(
-    "variable",
-    nargs=1,
-    type=click.STRING,
-)
-@click.argument("year", nargs=1, type=click.INT)
-@click.argument("month", nargs=1, type=click.INT)
-@click.option(
-    "--dry-run",
-    "dry_run",
-    is_flag=True,
-    show_default=True,
-    default=False,
-    help="Dry run, only prints the request. No download.",
-)
-def download_era5(variable: str, year: str, month: str, dry_run: bool) -> None:
-    """Download a ERA5 dataset.
-
-    VARIABLE: e.g. specific_humidity, u_component_of_wind
-
-    YEAR: e.g. 2020, 1987
-
-    MONTH: e.g. 5, 8. 0 for all months.
-
-    """
-    c = cdsapi.Client()
-    request = build_request(variable, year, month)
-
-    if not dry_run:
-        c.retrieve(
-            "reanalysis-era5-pressure-levels",
-            request,
-            # FIX: For now, pressure levels and area are fixed.
-            f"era5-{variable}-{year}_{month}-psl_1000_300-lat_90_40.nc",
-        )
-    else:
-        print(request)
 
 
 def generate_request_entries_from_specs(spec_paths):
--- cdsapi_helper/utils.py
+++ cdsapi_helper/utils.py
@@ -8,6 +8,8 @@ from typing import Dict, List, Union
 import pandas as pd
 
 REQUEST_DATABASE_FILE = Path("./cds_requests.csv")
+RE_FILENAMESPEC = re.compile(r"\{(\w+)\}")
+
 
 def format_bytes(size):
     power = 2**10
@@ -27,89 +29,12 @@ def print_files_and_size(filepaths: List[Path]):
     print(f"Files amount to {format_bytes(num_bytes)}.", file=sys.stderr)
 
 
-def build_request(
-    variable: str,
-    year: str,
-    month: str,
-    day: str = None,
-    pressure_levels: list[str] = None,
-    time_steps: list[str] = None,
-    area: list[str] = None,
-) -> dict:
-    year = str(year).zfill(2)
-
-    if month == 0:
-        month = ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12"]
-    else:
-        month = str(month).zfill(2)
-
-    if day is None:
-        day = [
-            "01",
-            "02",
-            "03",
-            "04",
-            "05",
-            "06",
-            "07",
-            "08",
-            "09",
-            "10",
-            "11",
-            "12",
-            "13",
-            "14",
-            "15",
-            "16",
-            "17",
-            "18",
-            "19",
-            "20",
-            "21",
-            "22",
-            "23",
-            "24",
-            "25",
-            "26",
-            "27",
-            "28",
-            "29",
-            "30",
-            "31",
-        ]
-    else:
-        day = str(day).zfill(2)
-    # Defaults.
-    if pressure_levels is None:
-        pressure_levels = ["300", "400", "500", "600", "700", "800", "900", "1000"]
-    if time_steps is None:
-        time_steps = ["00:00", "06:00", "12:00", "18:00"]
-    if area is None:
-        area = [90, -180, 40, 180]
-
-    request = {
-        "product_type": "reanalysis",
-        "format": "netcdf",
-        "variable": variable,
-        "pressure_level": pressure_levels,
-        "year": year,
-        "month": month,
-        "day": day,
-        "time": time_steps,
-        "area": area,
-    }
-    return request
-
-
 def request_to_df(request: dict, reply: dict, req_hash: str) -> pd.DataFrame:
     df = pd.DataFrame([request])
     df["request_hash"] = req_hash
     df["request_id"] = reply["request_id"]
     df["state"] = reply["state"]
     return df
-
-
-RE_FILENAMESPEC = re.compile(r"\{(\w+)\}")
 
 
 def build_filename(dataset: str, request: dict, filename_spec: str) -> str:
